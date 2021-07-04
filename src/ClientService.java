334294c1f516fc1a2fd4ddc09a16f75e1ec82044
Kevin <kevin.paul@johnabbottcollege.net>
2021-07-04 16:02:47 -0400

    Implement add(newItem) and getNext() methods from ICollection

--- src/ClientService.java
+++ src/ClientService.java
@@ -26,4 +26,23 @@ public class ClientService implements ICollection {
     public void setCap(int cap) {
         this.cap = cap;
     }
+
+    public boolean add(Object newItem) {
+        if (newItem instanceof Client && clients.size() < cap) {
+            Client clientToAdd = (Client) newItem;
+            clients.add(clientToAdd);
+            return true;
+        } else
+            return false;
+    }
+
+    public Object getNext() {
+        if (clients.size() > 0) {
+            Client next = clients.get(0);
+            clients.remove(0);
+            return next.getName();
+        } else
+            return false;
+    }
+
 }
