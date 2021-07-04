215e4516aeb67d9cd0f4eff8e532ce905de5c64a
Kevin <kevin.paul@johnabbottcollege.net>
2021-07-04 14:25:45 -0400

    Created Student class and DeansList class with constructors

---
+++ src/DeansList.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class DeansList {
+
+    ArrayList<Student> students;
+
+    public DeansList(ArrayList<Student> students) {
+        this.students = students;
+    }
+
+    public DeansList() {
+        this.students = new ArrayList<Student>();
+    }
+
+
+}
