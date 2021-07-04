3ec87044a738e73a302ba3d47a89837f860cedfa
Kevin <kevin.paul@johnabbottcollege.net>
2021-07-04 14:48:22 -0400

    Implemented getNext() and add(Object newItem) methods in DeansList

--- src/DeansList.java
+++ src/DeansList.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class DeansList {
+public class DeansList implements ICollection {
 
     private ArrayList<Student> students;
 
@@ -11,6 +11,41 @@ public class DeansList {
     public DeansList() {
         this.students = new ArrayList<Student>();
     }
+
+    public boolean add(Object newItem) {
+        if (newItem instanceof Student) {
+            Student studentToAdd = (Student) newItem;
+            if (studentToAdd.getGpa() >= 3.7) {
+                students.add(studentToAdd);
+                return true;
+            } else
+                return false;
+        }
+        else
+            return false;
+    }
+
+    public Object getNext() {
+        if (students.size() > 0) {
+            Student next = this.highestGpa();
+            students.remove(this.highestGpa());
+            return next;
+        } else
+            return false;
+    }
+
+    public Student highestGpa() {
+        int index = 0;
+        double max = 0.0;
+        for (int i = 0; i < students.size(); i++) {
+            if (students.get(i).getGpa() > max) {
+                max = students.get(i).getGpa();
+                index = i;
+            }
+        }
+        return students.get(index);
+    }
+
 
 
 }
